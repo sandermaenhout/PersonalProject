836a3b3811aeb8178917d32d91f56f7b2dbf4e83
irondeagle <30833805+irondeagle@users.noreply.github.com>
2019-12-10 17:08:40 +0100

    Setup project

---
+++ Assets/Scripts/MenuSceneUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneUI : MonoBehaviour
+{
+   public void SceneOptionsSwitch ()
+    {
+        Loader.Load(Loader.Scene.Options);
+    }
+
+    public void ScenePlayMenuSwitch()
+    {
+        Loader.Load(Loader.Scene.PlayMenu);
+    }
+
+    public void SceneShopSwitch()
+    {
+        Loader.Load(Loader.Scene.Shop);
+    }
+
+    public void SceneLeaderboardSwitch()
+    {
+        Loader.Load(Loader.Scene.Leaderboard);
+    }
+}
---
+++ Assets/Scripts/PreviousSceneButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousSceneButton : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        Loader.Load(Loader.Scene.Menu);
+    }
+
+    public void BackToShop()
+    {
+        Loader.Load(Loader.Scene.Shop);
+    }
+}
+
