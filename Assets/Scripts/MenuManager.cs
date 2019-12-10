b0e5e903aafc7dbf20338d4179525c179d6305f8
irondeagle <30833805+irondeagle@users.noreply.github.com>
2019-12-10 17:36:06 +0100

    Menu Scene created and linked to other scenes

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region UI Callback Methods
+
+    public void OnPlayButtonClicked()
+    {
+        //SceneManager.LoadScene("Scene_Loading");
+        SceneLoader.Instance.LoadScene("Scene_PlayMenu");
+    }
+
+    public void OnShopButtonClicked()
+    {
+        //SceneManager.LoadScene("Scene_Loading");
+        SceneLoader.Instance.LoadScene("Scene_Shop");
+    }
+
+    public void OnLeaderboardsButtonClicked()
+    {
+        //SceneManager.LoadScene("Scene_Loading");
+        SceneLoader.Instance.LoadScene("Scene_Leaderboard");
+    }
+
+    public void OnOptionsButtonClicked()
+    {
+        //SceneManager.LoadScene("Scene_Loading");
+        SceneLoader.Instance.LoadScene("Scene_Options");
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    private string sceneNameToBeLoaded;
+
+    public void LoadScene(string _sceneName)
+    {
+        sceneNameToBeLoaded = _sceneName;
+
+        StartCoroutine(InitializeSceneLoading());
+
+    }
+
+    IEnumerator InitializeSceneLoading()
+    {
+        yield return SceneManager.LoadSceneAsync("Scene_Loading");
+
+        StartCoroutine(LoadActuallyScene());
+
+    }
+
+    IEnumerator LoadActuallyScene()
+    {
+        var asyncSceneLoading = SceneManager.LoadSceneAsync(sceneNameToBeLoaded);
+
+        asyncSceneLoading.allowSceneActivation = false;
+
+        while (!asyncSceneLoading.isDone)
+        {
+            Debug.Log(asyncSceneLoading.progress + "%");
+            if (asyncSceneLoading.progress >= 0.9f)
+            {
+                asyncSceneLoading.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+    }
+}
